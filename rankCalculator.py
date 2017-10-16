256a2db2f063ffb24c49f0e70a5d709755d3f8c0
vamshiaruru <vamshiaruru0@gmail.com>
2017-10-17 00:21:40 +0530

    tempo commit

--- rankCalculator.py
+++ rankCalculator.py
@@ -2,9 +2,12 @@ from __future__ import division
 import numpy as np
 import scipy.sparse as ss
 
+taxation_factor = 0.85
 rows = np.load("rows.npy")
 cols = np.load("cols.npy")
 prob = np.load("prob.npy")
 rank_vector = np.full(34546, 1/34546)
+taxation_vector = np.ones(34546) * (1 - taxation_factor)/34546
 
-M = ss.csr_matrix((prob, (cols, rows)))
+M = ss.csr_matrix((prob, (cols, rows))) * taxation_factor
+
