cfd85dbf8ee6468055ad59798f32da6afc06d4cd
vamshiaruru <vamshiaruru0@gmail.com>
2017-10-16 14:39:49 +0530

    file reading done

---
+++ fileReader.py
@@ -0,0 +1,24 @@
+from __future__ import division
+import numpy as np
+import scipy
+import sys
+
+
+FILENAME = "data.txt"
+
+rows = np.zeros(421578)
+cols = np.zeros(421578)
+i = 0
+with open(FILENAME) as f:
+    for line in f:
+        line = ','.join(line.strip().split())
+        try:
+            row, col = line.split(",")
+            rows[i] = int(row)
+            cols[i] = int(col)
+            i = i + 1
+        except ValueError:
+            print "some thing went wrong"
+
+np.save("rows.npy", rows)
+np.save("cols.npy", cols)
